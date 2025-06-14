49558907777e0bb73cf611237b7b5b25f1e49c38
ggmz <ggmz0613@icloud.com>
2025-06-14 14:26:27 +0800

    feat(main):init go project

---
+++ app/test/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	json "github.com/json-iterator/go"
+)
+
+func main() {
+	aa := struct {
+		Name string `json:"name"`
+	}{
+		Name: "test",
+	}
+	ss, err := json.Marshal(aa)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s\n", string(ss))
+	fmt.Println("hello world")
+}
